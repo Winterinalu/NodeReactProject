ea1abe5a76f152028a7e869777caca19e1d5e952
Winterlu Armamentor <ZnCloud79@gmail.com>
2025-09-07 18:57:11 +0800

    2nd Update Website

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from './Navbar'
 import WinterLogo from './assets/Winter.png'
 import HekkLogo from './assets/Hekk.svg'
+import GithubPic from './assets/Github.png'
+import ReactPic from './assets/React.png'
+import VitePic from './assets/Vite.jpg'
 import './App.css'
 
 function App() {
@@ -27,21 +30,21 @@ function App() {
       <section className="features">
         <div className="feature">
           <a href="https://github.com/" target='_blank' className="feature-link">
-            <img className="feature-img" src="./src/assets/Github.png" alt="Fast feature" />
+            <img className="feature-img" src={GithubPic} alt="Fast feature" />
           </a>
           <h3>Github</h3>
           <p>It leverages Git, a distributed version control system, to allow multiple individuals to work on the same codebase simultaneously without overwriting each other's changes. It tracks every modification, enabling users to revert to previous versions, compare changes, and merge contributions seamlessly.</p>
         </div>
         <div className="feature">
           <a href="https://nodejs.org/en/" target='_blank' className="feature-link">
-            <img className="feature-img" src="./src/assets/React.png" alt="Minimal feature" />
+            <img className="feature-img" src={ReactPic} alt="Minimal feature" />
           </a>
           <h3>React</h3>
           <p>React is a JavaScript library primarily used for building user interfaces (UIs), particularly for single-page applications. Its core purpose is to facilitate the creation of interactive and dynamic web experiences by allowing developers to build UIs using a component-based architecture.</p>
         </div>
         <div className="feature">
           <a href="https://vite.dev/" target='_blank' className="feature-link">
-            <img className="feature-img" src="./src/assets/Vite.jpg" alt="Reliable feature" />
+            <img className="feature-img" src={VitePic} alt="Reliable feature" />
           </a>
           <h3>Vite</h3>
           <p>Vite is a build tool used in web development to provide a faster and leaner development experience by offering an instant dev server with hot module replacement and a production build command that uses Rollup to output optimized static assets.</p>
